e489d95122c026ec62352ba6ece52b9256bb73ca
Javier <jepach@gmail.com>
2024-03-22 23:46:37 -0500

    feat: add user signup and login features

---
+++ src/controllers/session.controller.js
@@ -0,0 +1,53 @@
+import bcrypt from "bcrypt";
+import { expressjwt } from "express-jwt";
+import jwt from "jsonwebtoken";
+import 'dotenv/config';
+import { createUser, getUserByEmail } from "./db/users.js";
+
+export const authMiddleware = expressjwt({
+    algorithms: ["HS256"],
+    credentialsRequired: false,
+    secret: process.env.JWT_SECRET,
+});
+  
+export async function handleLogin(req, res) {
+    const { email, password } = req.body;
+    
+    const user = await getUserByEmail(email)
+    if (!user) {
+      return res.status(401).json({ error: "Invalid email or password" });
+    }
+  
+    bcrypt.compare(password, user.password, (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: err });
+      }
+  
+      if (!result) {
+        return res.status(401).json({ error: "Invalid email or password" });
+      }
+  
+      const claims = { sub: user.id, email: user.email };
+      const token = jwt.sign(claims, process.env.JWT_SECRET, { expiresIn: "1h" });
+  
+      return res.status(200).json({ token });
+    });
+}
+  
+export async function handleSignup(req, res) {
+    const { name, email, password } = req.body;
+    const saltRounds = 10;
+  
+    bcrypt.hash(password, saltRounds, async (err, hash) => {
+      if (err) {
+        return res.status(500).json({ error: err });
+      }
+     
+      try {
+        const response = await createUser({ name, email, password: hash })
+        return res.status(200).json(response);
+      } catch {
+        return res.status(500).json({ error: "Error: User not created" });
+      }
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import { authMiddleware, handleLogin, handleSignup } from './controllers/session.controller';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors());
+app.use(cors(), bodyParser.json());
+
+app.post('/login', handleLogin);
+app.post('/signup', handleSignup);
+
+app.use(authMiddleware)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
