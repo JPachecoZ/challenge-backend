d6b2dd528201eab7c1587820e61873e394918eff
JPachecoZ <jepach@gmail.com>
2024-03-24 22:44:55 -0500

    feat: update yml

--- src/db/idgen.js
+++ src/db/idgen.js
@@ -3,3 +3,4 @@ import { customAlphabet } from "nanoid";
 const ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 export const generateId = customAlphabet(ALPHABET, 12);
+
